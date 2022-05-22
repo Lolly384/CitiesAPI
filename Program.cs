484714fa2e4d465b9ae8cbe833c59e420329adad
Lolly384 <PKA2711@mail.ru>
2022-05-22 22:45:04 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using CitiesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+string con = "Server=(localdb)\\mssqllocaldb;Database=usersdbstore;Trusted_Connection=True;";
+
+builder.Services.AddDbContext<CitiesContext>(options =>
+    options.UseSqlServer(con));
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints => {
+    endpoints.MapControllers();
+});
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.Run();
+
